292065252e20649444b15a6a9f75ed9c0600dd5e
Esat-Karakaya <esat1karakaya@gmail.com>
2023-12-03 20:53:24 +0300

    suggester available

--- components/routes/gamesScreen.js
+++ components/routes/gamesScreen.js
@@ -28,25 +28,38 @@ function GameMenu({ navigation, minParent, normParent }) {
     const readItems = rawData === null ? [] : JSON.parse(rawData)//parse to arr
     setMistakesAtom(readItems)
     //setItem(storage, "[]") TO RESET
-    
-    setNextGameAtom({ get: (isRandom ? () => gamePicker(true) : () => goToGame(route, storage, false, true)) })
+
+    setNextGameAtom({ get: (isRandom ? () => randomNavigator(true) : () => goToGame(route, storage, false, true)) })
     navigation[shouldReplace ? "replace" : "navigate"](route);
     minParent()
   }
 
-  const gamePicker = async (bool) => {//SIDE EFFECTS
+  const randomNavigator = async (bool) => {//SIDE EFFECTS
     const rawData = await getItem("falseAndTotal")
-    const statistics=rawData ? JSON.parse(rawData) : { emojisGame: [5, 5], mazeGame: [5, 5], passwordCracking: [5, 5] }//parse to obj
-    
-    if (rawData===null) {
+    const statistics = rawData ? JSON.parse(rawData) : { emojisGame: [5, 5], mazeGame: [5, 5], passwordCracking: [5, 5] }//parse to obj
+
+    if (rawData === null) {
       setItem("falseAndTotal", JSON.stringify(statistics))
     }
-    
-    const pickedNum = Math.ceil(Math.random() * 100)
+
+    const pickedNum = Math.random() * 100
     if (pickedNum > 50) {
-      goToGame("Emojileri Hatırla", "emojisGameMistakes", true, bool)
     } else {
-      goToGame("Labirentten Çıkış", "emojisGameMistakes", true, bool)
+
+    }
+
+    switch (pickedGame(gamePercentages(statistics))) {
+      case "emojisGame":
+        goToGame("Emojileri Hatırla", "emojisGameMistakes", true, bool)
+        break;
+
+      case "mazeGame":
+        goToGame("Labirentten Çıkış", "emojisGameMistakes", true, bool)
+        break;
+
+      case "passwordCracking":
+        goToGame("Şifre Kırma", "emojisGameMistakes", true, bool)
+        break;
     }
   }
 
@@ -62,7 +75,7 @@ function GameMenu({ navigation, minParent, normParent }) {
         onPress={() => goToGame("Şifre Kırma", "emojisGameMistakes")}
         GameTitle={"Şifre Kırma"} />
       <GameLink GameIcon={"✏️"}
-        onPress={() => gamePicker()}
+        onPress={() => randomNavigator()}
         GameTitle={"Özelleştirilmiş Test"} />
     </View>
   )
@@ -90,4 +103,40 @@ export default function GamesScreen({ navigation }) {
       <Stack.Screen name="Şifre Kırma" component={PasswordCracking} />
     </Stack.Navigator>
   )
+}
+
+const gamePercentages = (statistics) => {
+  const probabilities = {}
+
+  const sum = Object.entries(statistics).reduce((acc, gameInfo) => {
+    const inaccuracy = gameInfo[1][0] / gameInfo[1][1]
+    probabilities[gameInfo[0]] = inaccuracy
+
+    return inaccuracy + acc
+  }, 0)
+
+  for (const key in probabilities) {
+    probabilities[key] = probabilities[key] * 100 / sum
+  }
+
+  return probabilities
+}
+
+const pickedGame = (probabilities) => {
+  const minVals = []
+  for (const key in probabilities) {
+    minVals.push((minVals.at(-1) ?? 0) + probabilities[key])
+  }
+  const pickedNum = Math.random() * 100
+
+  let pickedI = 0
+  minVals.every((val, i) => {
+    if (val >= pickedNum) {
+      pickedI = i
+      return false
+    }
+    return true
+  })
+
+  return Object.keys(probabilities)[pickedI]
 }
