06ab8a65fd9a0fde91a1a1d293a5448efcbaf0a5
Esat-Karakaya <esat1karakaya@gmail.com>
2023-12-16 14:17:21 +0300

    simplify

--- components/routes/parrotScreen.js
+++ components/routes/parrotScreen.js
@@ -1,7 +1,54 @@
-import ParrotKeep from '../parrotKeep'
+import { StyleSheet, View, Image, Dimensions } from "react-native";
+import OptionButton from "../simpleComponents/customButton"
+import Img from "../../assets/parrot.png"
+import { useState } from "react";
 
 export default ParrotScreen=()=>{
+  const [selecteds, setSelecteds]=useState([])
   return (
-    <ParrotKeep/>
+    <View style={styles.pageContainer}>
+      <Image style={styles.parrot} source={Img}/>
+      <View style={styles.buttonContain}>
+        <OptionButton
+          onPress={()=>setSelecteds(arr=>["🫐", ...arr])}
+          style={
+            selecteds.includes("🫐")?
+            {display:"none"}:
+            {}
+          }
+          children={"🫐"}/>
+        <OptionButton
+          onPress={()=>setSelecteds(arr=>["💧", ...arr])}
+          style={
+            selecteds.includes("💧")?
+            {display:"none"}:
+            {}
+          }
+          children={"💧"}/>
+      </View>
+    </View>
   );
 }
+
+const styles=StyleSheet.create({
+  parrot:{
+    marginTop:40,
+    width:Dimensions.get("window").width*3/4,
+    height:Dimensions.get("window").width*3/4,
+    borderRadius:20,
+  },
+  pageContainer:{
+    flex:1,
+    justifyContent:"space-between",
+    alignItems:"center",
+
+  },
+  buttonContain:{
+    width:"100%",
+    flexDirection:"row",
+    justifyContent:"space-around",
+    backgroundColor:"aqua",
+    padding:10,
+    borderTopWidth:3,
+  }
+})
