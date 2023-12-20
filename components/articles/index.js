a87b7c2ee140e2cb3b366b8a30ac5e7583dfe096
Esat-Karakaya <esat1karakaya@gmail.com>
2023-12-20 18:11:57 +0300

    fixed tips bugs

--- components/articles/index.js
+++ components/articles/index.js
@@ -1,13 +1,13 @@
-import { StyleSheet, Text } from "react-native"
+import { StyleSheet, Text, ScrollView } from "react-native"
 import { PARAGRAPHS, HEADERS } from "../../globals"
 
 export default function Paragraph({ route, navigation }) {
     navigation.setOptions({ title:HEADERS[route.params.tipId] })
 
     return(
-        <>
+        <ScrollView>
             <Text style={styles.paragraph}> { PARAGRAPHS[route.params.tipId] } </Text>
-        </>
+        </ScrollView>
     )
 }
 
